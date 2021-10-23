a3e798deec00a9504294de27e4a0d1132c04dcd4
Shuting Li <sli2@oxy.edu>
2021-10-22 21:13:22 -0700

    commit on 'main'

--- frontend2/src/App.js
+++ frontend2/src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
-import {BrowserRouter, Link} from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { BrowserRouter, Link } from 'react-router-dom';
 import { Route } from 'react-router-dom';
+import { signout } from './actions/userActions';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import SigninScreen from './screens/SigninScreen';
@@ -11,36 +12,48 @@ import SigninScreen from './screens/SigninScreen';
 
 function App() {
   const userSignin = useSelector((state) => state.userSignin);
-  const {userInfo} = userSignin; 
+  const { userInfo } = userSignin;
+  const dispatch = useDispatch();
+  const signoutHandler = () => {
+    dispatch(signout());
+  };
   return (
     <BrowserRouter>
-    <div className="grid-container">
-      <header className="row">
-        <div>
-          <a className="brand" href="/">
-            OxyTradePost
-          </a>
-        </div>
-        <div>
-          {
-            userInfo ? (
-              <Link to ="#">{userInfo.name}</Link>
-            ): (
-              <Link to = "/signin">Sign In</Link>
-            )
-          }
-          
+      <div className="grid-container">
+        <header className="row">
+          <div>
+            <a className="brand" href="/">
+              OxyTradePost
+            </a>
+          </div>
+          <div>
+            {
+              userInfo ? (
+                <div className="dropdown">
+                  <Link to="#">{userInfo.name} <i className="fa fa-caret-down"></i> </Link>
+                
+                <ul className = "dropdown-content">
+                  <Link to="#signout" onClick={signoutHandler}>
+                      Sign Out
+                    </Link>
+                </ul>
+                </div>
+              ) : (
+                <Link to="/signin">Sign In</Link>
+              )
+            }
 
-        </div>
-      </header>
-      <main>
-        <Route path="/product/:id" component={ProductScreen}></Route>
-        <Route path="/" component={HomeScreen} exact></Route>
-        <Route path="/signin" component={SigninScreen}></Route>
-        
-      </main>
-      <footer className="row center">All right reserved</footer>
-    </div>
+
+          </div>
+        </header>
+        <main>
+          <Route path="/product/:id" component={ProductScreen}></Route>
+          <Route path="/" component={HomeScreen} exact></Route>
+          <Route path="/signin" component={SigninScreen}></Route>
+
+        </main>
+        <footer className="row center">All right reserved</footer>
+      </div>
     </BrowserRouter>
   );
 }
