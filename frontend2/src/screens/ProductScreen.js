f176a46456b1f7bd3b916a682cafab9d17d394ae
Shuting Li <sli2@oxy.edu>
2021-10-25 20:46:28 -0700

    commit on 'main'

--- frontend2/src/screens/ProductScreen.js
+++ frontend2/src/screens/ProductScreen.js
@@ -32,7 +32,7 @@ export default function ProductScreen(props) {
                     alt={product.name}
                   ></img>
                 </div>
-                <div className="col-1">
+                <div className="col-2">
                   <ul>
                     <li>
                       <h1>{product.name}</h1>
@@ -42,34 +42,10 @@ export default function ProductScreen(props) {
                       Description:
                       <p>{product.description}</p>
                     </li>
-                  </ul>
-                </div>
-                <div className="col-1">
-                  <div className="card card-body">
-                    <ul>
-                      <li>
-                        <div className="row">
-                          <div>Price</div>
-                          <div className="price">${product.price}</div>
-                        </div>
-                      </li>
-                      <li>
-                        <div className="row">
-                          <div>Status</div>
-                          <div>
-                            {product.countInStock > 0 ? (
-                              <span className="success">In Stock</span>
-                            ) : (
-                              <span className="danger">Unavailable</span>
-                            )}
-                          </div>
-                        </div>
-                      </li>
-                      <li>
+                    <li>
                         <button className="primary block">Message Seller</button>
                       </li>
-                    </ul>
-                  </div>
+                  </ul>
                 </div>
               </div>
             </div>
