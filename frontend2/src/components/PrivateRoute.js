209a45800273f735267cf4e45f781ba231a66b08
Shuting Li <sli2@oxy.edu>
2021-12-06 13:03:08 -0800

    commit to 'chat'

--- frontend2/src/components/PrivateRoute.js
+++ frontend2/src/components/PrivateRoute.js
@@ -3,9 +3,13 @@ import { useSelector } from 'react-redux';
 import { Redirect, Route } from 'react-router-dom';
 
 export default function PrivateRoute({ component: Component, ...rest }) {
+  // component: the priavte component get passed in in App.js
+  // ...rest the rest of props
   const userSignin = useSelector((state) => state.userSignin);
   const { userInfo } = userSignin;
   return (
+    // Show the component only when the user is logged in
+    // Otherwise, redirect the user to /signin page
     <Route
       {...rest}
       render={(props) =>
